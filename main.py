56dae9579fcfd080833f510a5248912c4b34fec1
Jackson Lin <jackson1994y+git@gmail.com>
2018-10-17 17:03:11 -0400

    Complete clean function

--- main.py
+++ main.py
@@ -10,7 +10,41 @@ logger.setLevel('DEBUG')
 def clean(inputFile, mappingFile):
     logger.info('Cleaning file {} with mapping data {}...'.format(
         inputFile, mappingFile))
-    pass
+
+    # Generate mapping and reversMapping dictionary
+    logger.debug('Generating mapping dictionary')
+    mapping={}
+    reversMapping={}
+    with open(mappingFile,'r') as inputF:
+        for line in inputF:
+            index, name = line.split()
+            mapping[index] = name
+            reversMapping[name] = index
+    logger.debug('mapping: {}'.format(mapping))
+    logger.debug('reversMapping: {}'.format(reversMapping))
+    logger.debug('Mapping created')
+
+    # Start to clean up transaction with mapping & reverseMapping dict.
+    logger.debug('Start to clean file')
+    cleanedFile = 'tempCleaned.txt' # Store the temprary cleaned file in 'tempCleaned.txt'
+    with open(cleanedFile,'w+') as output:  # Open temporary cleaned file
+        line = ''
+        with open(inputFile,'r') as inputF: # Open uncleaned input file
+            for line in inputF:
+                keep = []
+                line = line.replace(';',' ')    # Replace every semicolumn with space
+                elements = line.split()         # Seperate element with space
+                logger.debug('Original elements: len({}) data:{}'.format(len(elements),elements))
+                for element in elements:        # Loop through ever element in one line(one transaction)
+                    if element in mapping:      # If element is a valid PXX format inside mappingFile given
+                        keep.append(element)
+                    elif element in reversMapping: # If element is listed as String in mappingFile then reverse it to PXX format
+                        keep.append(reversMapping[element])
+                logger.debug('Cleaned elements: len({}) data:{}'.format(len(keep),keep))
+                output.write(' '.join(keep)+'\n') # Save cleaned transaction in to cleanedfile
+    # Filed cleaned
+    logger.info('File cleaned, storing cleaned file at {}.'.format(cleanedFile))
+    return cleanedFile
 
 
 def apriori(inputFile, outputFile, minimumSupport):
@@ -45,4 +79,5 @@ if __name__ == '__main__':
         outpuFile = sys.argv[4]
         logger.info('Processing with dirty file {}'.format(dirtyFile))
         cleanedFile = clean(dirtyFile, mappingFile)
+        logger.debug('Using cleaned file {}'.format(cleanedFile))
         apriori(cleanedFile, outpuFile, minimumSupport)
